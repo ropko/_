1dd41723b3fac515e1902a8b079527f0f1d8452a
ropko <43463707+ropko@users.noreply.github.com>
2019-02-28 19:04:35 +0100

    Add files via upload

---
+++ assets/html/main.js
@@ -0,0 +1,44 @@
+
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+
+  fill.addEventListener('dragstart', dragStart, false);`
+  fill.addEventListener('dragend', dragEnd, true );
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+
+function dragStart() {
+  console.log('start')
+  this.className += ' hold';
+  setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd() {
+  this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+}
+
+function dragLeave() {
+  this.className = 'empty';
+}
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
+ }
